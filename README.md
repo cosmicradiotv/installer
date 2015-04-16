866721a32566092cb5b158a466805c6a5ab2b18c
t2t2 <taavot@gmail.com>
2015-04-16 09:20:37 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿Quick installer for setting up the environment for CRTV development. Installs everything into crtv directory and clones plugin and theme repository.
+
+Usage: `./install.sh`
+
+By default git clone uses SSH. If you don't have SSH keys set up you can clone read-only mode over https by running `./install.sh https`
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+command -v git >/dev/null 2>&1 || { echo >&2 "Can't find git. Aborting"; exit 1; }
+command -v php >/dev/null 2>&1 || { echo >&2 "Can't find php (cli)). Aborting"; exit 1; }
+command -v composer >/dev/null 2>&1 || { echo >&2 "Can't find composer. Aborting"; exit 1; }
+
+if [[ "$1" != "https" ]]; then
+    PLUGIN_PODCAST_URL="git@github.com:cosmicradiotv/podcast-plugin.git"
+	THEME_URL="git@github.com:cosmicradiotv/u62.git"
+else
+    PLUGIN_PODCAST_URL="https://github.com/cosmicradiotv/podcast-plugin.git"
+	THEME_URL="https://github.com/cosmicradiotv/u62.git"
+fi
+
+composer create-project october/october crtv dev-master --prefer-dist
+git clone "$PLUGIN_PODCAST_URL" "crtv/plugins/cosmicradiotv/podcast"
+git clone "$THEME_URL" "crtv/themes/u82"
+
+echo ""
+echo "Cloned!"
+echo ""
+echo "Follow the instructions *after the composer step*:"
+echo "http://octobercms.com/docs/help/console#console-install"
