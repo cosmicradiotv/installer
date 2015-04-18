8e46a31d4df38c6e782e02e96b7a3253cb5cecb8
Roberto Villegas <vincent404@gmail.com>
2015-04-18 08:21:26 -0500

    Fixed URL and Typo
    Updated the URLs and fixed a typo in the theme URL.

--- install.sh
+++ install.sh
@@ -6,15 +6,15 @@ command -v composer >/dev/null 2>&1 || { echo >&2 "Can't find composer. Aborting
 
 if [[ "$1" != "https" ]]; then
 	PLUGIN_PODCAST_URL="git@github.com:cosmicradiotv/podcast-plugin.git"
-	THEME_URL="git@github.com:cosmicradiotv/u62.git"
+	THEME_URL="git@github.com:cosmicradiotv/u62-theme.git"
 else
 	PLUGIN_PODCAST_URL="https://github.com/cosmicradiotv/podcast-plugin.git"
-	THEME_URL="https://github.com/cosmicradiotv/u62.git"
+	THEME_URL="https://github.com/cosmicradiotv/u62-theme.git"
 fi
 
 composer create-project october/october crtv dev-master --prefer-dist
 git clone "$PLUGIN_PODCAST_URL" "crtv/plugins/cosmicradiotv/podcast"
-git clone "$THEME_URL" "crtv/themes/u82"
+git clone "$THEME_URL" "crtv/themes/u62"
 
 echo ""
 echo "Cloned!"
